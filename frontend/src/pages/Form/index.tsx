c58116b2f4a20e71b40e8a8697bf4c5d2788fe97
Moises Ezequiel <contato.moisesezequiel@outlook.com>
2022-01-15 23:35:51 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,7 @@
+function Form(){
+    return (
+        <h1>PAGINA DE FORMULARIOO</h1>
+    );
+
+}
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,7 @@
+function Listing(){
+    return (
+        <h1>PAGINA DE LISTAGEM</h1>
+    );
+
+}
+export default Listing;
