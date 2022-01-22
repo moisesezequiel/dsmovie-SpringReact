54fe7a33a14f3c35715c96abf73af884db72394c
Moises Ezequiel <contato.moisesezequiel@outlook.com>
2022-01-22 12:56:46 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -25,7 +25,7 @@ function Listing() {
 
     useEffect(() => {
 
-        axios.get(`${BASE_URL}/movies?size=60&page=${pageNumber}&sort=title`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
             .then(response => {
                 const data = response.data as MoviePage;
                 setPage(data)
@@ -33,18 +33,13 @@ function Listing() {
 
     }, [pageNumber]);
 
-    const movie = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-    };
-
+    const handlePageChange = (newPageNumber : number) =>{
+        setPageNumber(newPageNumber);
+    }
 
     return (
         <>
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
             <div className="container">
                 <div className="row">
 
